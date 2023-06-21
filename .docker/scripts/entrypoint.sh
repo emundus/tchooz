784fc73691934d1528b3e9b19470b7f09dbf57a4
Brice <b.hubinet@gmail.com>
2023-06-21 11:40:46 +0200

    Add Joomla core

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -128,21 +128,41 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 
     cp /templates/g5_helium/templateDetails.xml templates/g5_helium/templateDetails.xml
     cp -r /templates/g5_helium/custom/config templates/g5_helium/custom/config
+  fi
 
-    if ["$INIT_DATABASE" == 'true']; then
-      echo >&2 "We prepare the installation of the Tchooz component..."
+  if [ ! -e configuration.php && -d ".docker/installation/" ]; then
 
-      php cli/joomla.php database:import --folder=".docker/installation/core/vanilla"
-
-      echo >&2 "Create coordinator user..."
-
-      php cli/joomla.php user:add --username="$TCHOOZ_COORD_USERNAME" --name="$TCHOOZ_COORD_LAST_NAME $TCHOOZ_COORD_FIRST_NAME" --password="$TCHOOZ_COORD_PASSWORD" --email="$TCHOOZ_COORD_MAIL" --usergroup="Registered,Administrator"
-    fi
+    echo >&2 "We prepare the installation of the Tchooz component..."
 
     echo >&2 "Init configuration variables..."
+    cp configuration.php.dist configuration.php
+    cp .htaccess.txt .htaccess
 
-    php cli/joomla.php config:set sef_rewrite=true
-    php cli/joomla.php config:set frontediting=0
+    sed -i "s:\$host = '.*':\$host = '$JOOMLA_DB_HOST':g" configuration.php
+    sed -i "s:\$user = '.*':\$user = '$JOOMLA_DB_USER':g" configuration.php
+    sed -i "s:\$password = '.*':\$password = '$JOOMLA_DB_PASSWORD':g" configuration.php
+    sed -i "s:\$db = '.*':\$db = '$JOOMLA_DB_NAME':g" configuration.php
+
+    chown www-data: configuration.php
+
+    php cli/joomla.php config:set sitename="$TCHOOZ_SITENAME" dbtype="mysqli" sef_rewrite=true frontediting=0
+
+    echo >&2 "Init database..."
+
+    php cli/joomla.php database:import --folder=".docker/installation/core/vanilla" -n
+
+
+    echo >&2 "Create super administrator user..."
+
+    php cli/joomla.php user:add --username="$TCHOOZ_SYSADMIN_USERNAME" --name="$TCHOOZ_SYSADMIN_LAST_NAME $TCHOOZ_SYSADMIN_FIRST_NAME" --password="$TCHOOZ_SYSADMIN_PASSWORD" --email="$TCHOOZ_SYSADMIN_MAIL" --usergroup="Registered,Super Users" -n
+
+
+    echo >&2 "Create coordinator user..."
+
+    php cli/joomla.php user:add --username="$TCHOOZ_COORD_USERNAME" --name="$TCHOOZ_COORD_LAST_NAME $TCHOOZ_COORD_FIRST_NAME" --password="$TCHOOZ_COORD_PASSWORD" --email="$TCHOOZ_COORD_MAIL" --usergroup="Registered,Administrator" -n
+
+
+    chown -R www-data: .
 
     echo >&2 "Awesome ! Your Tchooz website is ready !"
   fi
