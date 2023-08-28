a1360d928993298fcc9afd121641afa1a1904bd8
Brice <b.hubinet@gmail.com>
2023-08-28 17:29:34 +0200

    Fix login page

--- templates/g5_helium/html/com_users/login/default_login.php
+++ templates/g5_helium/html/com_users/login/default_login.php
@@ -1,78 +1,76 @@
 <?php
+
 /**
  * @package     Joomla.Site
  * @subpackage  com_users
  *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('_JEXEC') or die;
-JHtml::_('behavior.keepalive');
-JHtml::_('behavior.formvalidator');
-$document = JFactory::getDocument();
-$document->addStyleSheet("templates/g5_helium/html/com_users/login/style/com_users_login.css");
-$eMConfig = JComponentHelper::getParams('com_emundus');
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Router\Route;
+
+/** @var \Joomla\Component\Users\Site\View\Login\HtmlView $cookieLogin */
+
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $this->document->getWebAssetManager();
+$wa->useScript('keepalive')
+    ->useScript('form.validate');
+$wa->registerAndUseStyle('com_users.login', 'templates/g5_helium/html/com_users/login/style/com_users_login.css');
+
+$usersConfig = ComponentHelper::getParams('com_users');
+$eMConfig = ComponentHelper::getParams('com_emundus');
+
+$session = JFactory::getApplication()->getSession();
 
 if(!empty($this->campaign)){
-    JFactory::getSession()->set('login_campaign_id',$this->campaign);
+    $session->set('login_campaign_id',$this->campaign);
 } else {
-	JFactory::getSession()->clear('login_campaign_id');
+    $session->clear('login_campaign_id');
 }
 ?>
-<div class="login<?php echo $this->pageclass_sfx; ?>">
+<div class="com-users-login login">
     <?php if ($this->params->get('show_page_heading')) : ?>
-        <div class="page-header">
-            <?php if (file_exists('images/custom/favicon.png')) : ?>
-                <a href="index.php" alt="Logo" class="em-profile-picture mb-8" style="width: 50px;height: 50px;background-image: url('images/custom/favicon.png')">
-                </a>
-            <?php endif; ?>
-            <h1 class="em-mb-8">
-                <?php echo JText::_('JLOGIN'); ?>
-            </h1>
-            <p class="em-applicant-text-color em-applicant-default-font"><?php echo JText::_('JLOGIN_DESC'); ?></p>
-        </div>
-    <?php endif; ?>
-    <?php if (($this->params->get('logindescription_show') == 1 && str_replace(' ', '', $this->params->get('login_description')) != '') || $this->params->get('login_image') != '') : ?>
-    <div class="login-description">
+    <div class="page-header flex flex-column items-center">
+        <?php if (file_exists('images/custom/favicon.png')) : ?>
+            <a href="index.php" alt="Logo" class="em-profile-picture mb-2" style="width: 50px;height: 50px;background-image: url('images/custom/favicon.png')">
+            </a>
         <?php endif; ?>
+        <h1 class="mb-3">
+            <?php echo JText::_('JLOGIN'); ?>
+        </h1>
+    </div>
+    <?php endif; ?>
+
+    <?php if (($this->params->get('logindescription_show') == 1 && str_replace(' ', '', $this->params->get('login_description', '')) != '') || $this->params->get('login_image') != '') : ?>
+    <div class="com-users-login__description login-description">
+    <?php endif; ?>
+
         <?php if ($this->params->get('logindescription_show') == 1) : ?>
             <?php echo $this->params->get('login_description'); ?>
         <?php endif; ?>
+
         <?php if ($this->params->get('login_image') != '') : ?>
-            <img src="<?php echo $this->escape($this->params->get('login_image')); ?>" class="login-image" alt="<?php echo JText::_('COM_USERS_LOGIN_IMAGE_ALT'); ?>" />
+            <?php echo HTMLHelper::_('image', $this->params->get('login_image'), empty($this->params->get('login_image_alt')) && empty($this->params->get('login_image_alt_empty')) ? false : $this->params->get('login_image_alt'), ['class' => 'com-users-login__image login-image']); ?>
         <?php endif; ?>
-        <?php if (($this->params->get('logindescription_show') == 1 && str_replace(' ', '', $this->params->get('login_description')) != '') || $this->params->get('login_image') != '') : ?>
+
+    <?php if (($this->params->get('logindescription_show') == 1 && str_replace(' ', '', $this->params->get('login_description', '')) != '') || $this->params->get('login_image') != '') : ?>
     </div>
-<?php endif; ?>
-    <form id="login_form" action="<?php echo (!empty($this->redirect)) ? 'index.php?option=com_users&task=user.login&redirect='.$this->redirect : 'index.php?option=com_users&task=user.login'; ?>" method="post" class="form-validate form-horizontal well">
+    <?php endif; ?>
+
+    <form action="<?php echo (!empty($this->redirect)) ? 'index.php?option=com_users&task=user.login&redirect='.$this->redirect : 'index.php?option=com_users&task=user.login'; ?>" method="post" class="form-validate form-horizontal well" id="com-users-login__form">
+
         <fieldset>
-            <?php foreach ($this->form->getFieldset('credentials') as $field) : ?>
-                <?php if (!$field->hidden) : ?>
-                    <div class="control-group mb-8">
-                        <div class="control-label">
-                            <?php echo $field->label; ?>
-                        </div>
-                        <div class="controls" style="<?= $field->type === "Password" ? 'position:relative; ' : '' ?>">
-                            <?php echo $field->input; ?>
-                            <?php if ($eMConfig["reveal_password"] && $field->type === "Password"): ?>
-                                <span id="toggle-password-visibility" class="material-icons-outlined em-pointer" style="position: absolute;margin-top: 4px;right: 10px;opacity: 0.3;user-select: none;">visibility_off</span>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            <?php endforeach; ?>
-            <?php if ($this->tfa) : ?>
-                <div class="control-group">
-                    <div class="control-label">
-                        <?php echo $this->form->getField('secretkey')->label; ?>
-                    </div>
-                    <div class="controls">
-                        <?php echo $this->form->getField('secretkey')->input; ?>
-                    </div>
-                </div>
-            <?php endif; ?>
+            <?php echo $this->form->renderFieldset('credentials', ['class' => 'com-users-login__input']); ?>
+
             <div class="em-w-100 em-flex-row em-flex-end">
-                <?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
+                <?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
                     <div class="control-group">
                         <div class="control-label">
                             <label for="remember">
@@ -84,61 +82,69 @@ if(!empty($this->campaign)){
                         </div>
                     </div>
                 <?php endif; ?>
+
                 <?php if($this->displayForgotten) : ?>
-                <div class="control-group em-float-right">
-                    <div class="control-label">
-                        <a class="em-text-underline" href="<?php echo JRoute::_($this->forgottenLink); ?>">
-                            <?php echo JText::_('COM_USERS_LOGIN_RESET'); ?>
-                        </a>
+                    <div class="control-group em-float-right">
+                        <div class="control-label">
+                            <a class="em-text-underline" href="<?php echo JRoute::_($this->forgottenLink); ?>">
+                                <?php echo JText::_('COM_USERS_LOGIN_RESET'); ?>
+                            </a>
+                        </div>
                     </div>
-                </div>
                 <?php endif; ?>
             </div>
-            <div class="control-group em-w-100">
+
+            <?php foreach ($this->extraButtons as $button) :
+                $dataAttributeKeys = array_filter(array_keys($button), function ($key) {
+                    return substr($key, 0, 5) == 'data-';
+                });
+                ?>
+                <div class="com-users-login__submit control-group">
+                    <div class="controls">
+                        <button type="button"
+                                class="btn btn-secondary w-100 <?php echo $button['class'] ?? '' ?>"
+                                <?php foreach ($dataAttributeKeys as $key) : ?>
+                                    <?php echo $key ?>="<?php echo $button[$key] ?>"
+                                <?php endforeach; ?>
+                                <?php if ($button['onclick']) : ?>
+                                onclick="<?php echo $button['onclick'] ?>"
+                                <?php endif; ?>
+                                title="<?php echo Text::_($button['label']) ?>"
+                                id="<?php echo $button['id'] ?>"
+                        >
+                            <?php if (!empty($button['icon'])) : ?>
+                                <span class="<?php echo $button['icon'] ?>"></span>
+                            <?php elseif (!empty($button['image'])) : ?>
+                                <?php echo HTMLHelper::_('image', $button['image'], Text::_($button['tooltip'] ?? ''), [
+                                    'class' => 'icon',
+                                ], true) ?>
+                            <?php elseif (!empty($button['svg'])) : ?>
+                                <?php echo $button['svg']; ?>
+                            <?php endif; ?>
+                            <?php echo Text::_($button['label']) ?>
+                        </button>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+            <div class="com-users-login__submit control-group">
                 <div class="controls">
-                    <button type="submit" class="em-applicant-primary-button w-full em-applicant-border-radius">
-                        <?php echo JText::_('JLOGIN'); ?>
+                    <button type="submit" class="btn btn-primary">
+                        <?php echo Text::_('JLOGIN'); ?>
                     </button>
                 </div>
             </div>
-            <?php $return = $this->form->getValue('return', '', $this->params->get('login_redirect_url', $this->params->get('login_redirect_menuitem'))); ?>
-            <input type="hidden" name="return" value="<?php echo base64_encode($return); ?>" />
-            <?php echo JHtml::_('form.token'); ?>
+
+            <?php $return = $this->form->getValue('return', '', $this->params->get('login_redirect_url', $this->params->get('login_redirect_menuitem', ''))); ?>
+            <input type="hidden" name="return" value="<?php echo base64_encode($return); ?>">
+            <?php echo HTMLHelper::_('form.token'); ?>
         </fieldset>
     </form>
-
-    <?php $usersConfig = JComponentHelper::getParams('com_users'); ?>
-    <?php if ($usersConfig->get('allowUserRegistration') && $this->displayRegistration) : ?>
-        <div>
-            <?php echo JText::_('COM_USERS_LOGIN_NO_ACCOUNT'); ?>
-            <a class="em-text-underline" href="<?php echo JRoute::_($this->registrationLink); ?>">
-                <?php echo JText::_('COM_USERS_LOGIN_REGISTER'); ?>
+    <div class="com-users-login__options list-group">
+        <?php if ($usersConfig->get('allowUserRegistration')) : ?>
+            <a class="com-users-login__register list-group-item" href="<?php echo Route::_('index.php?option=com_users&view=registration'); ?>">
+                <?php echo Text::_('COM_USERS_LOGIN_REGISTER'); ?>
             </a>
-        </div>
-    <?php endif; ?>
-</div>
-
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        document.querySelector('#header-a img').style.display = 'none';
-
-        <?php if ($eMConfig['reveal_password']): ?>
-            const spanVisibility = document.querySelector('#toggle-password-visibility');
-            const inputPassword = document.querySelector('.controls #password');
-
-            if (spanVisibility && inputPassword) {
-                spanVisibility.addEventListener('click', function () {
-                    if (spanVisibility && inputPassword) {
-                        if (spanVisibility.innerText == "visibility") {
-                            spanVisibility.innerText = "visibility_off";
-                            inputPassword.type = "password";
-                        } else {
-                            spanVisibility.innerText = "visibility";
-                            inputPassword.type = "text";
-                        }
-                    }
-                });
-            }
         <?php endif; ?>
-    });
-</script>
+    </div>
+</div>
