9efda14728840d900bcad9d0fcbcf24a019e1e26
Legendre <jeremy.legendre@emundus.fr>
2023-09-26 16:19:58 +0200

    update unit tests

--- components/com_emundus/unittest/EmundusHelperEmail.php
+++ components/com_emundus/unittest/EmundusHelperEmail.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use PHPUnit\Framework\TestCase;
 ini_set( 'display_errors', false );
 error_reporting(E_ALL);
@@ -8,23 +7,23 @@ define('_JEXEC', 1);
 define('DS', DIRECTORY_SEPARATOR);
 define('JPATH_BASE', dirname(__DIR__) . '/../../');
 
-include_once ( JPATH_BASE . 'includes/defines.php' );
-include_once ( JPATH_BASE . 'includes/framework.php' );
-include_once(JPATH_SITE.'/components/com_emundus/unittest/helpers/samples.php');
-include_once (JPATH_SITE . '/components/com_emundus/helpers/emails.php');
+include_once(JPATH_BASE . 'includes/defines.php');
+include_once(JPATH_BASE . 'includes/framework.php');
+include_once(JPATH_SITE . '/components/com_emundus/unittest/helpers/samples.php');
+include_once(JPATH_SITE . '/components/com_emundus/helpers/emails.php');
 
-jimport('joomla.user.helper');
-jimport( 'joomla.application.application' );
-jimport('joomla.plugin.helper');
+// Boot the DI container
+$container = \Joomla\CMS\Factory::getContainer();
+$container->alias('session.web', 'session.web.site')
+	->alias('session', 'session.web.site')
+	->alias('JSession', 'session.web.site')
+	->alias(Joomla\CMS\Session\Session::class, 'session.web.site')
+	->alias(Joomla\Session\Session::class, 'session.web.site')
+	->alias(Joomla\Session\SessionInterface::class, 'session.web.site');
 
-// set global config --> initialize Joomla Application with default param 'site'
-JFactory::getApplication('site');
+// Instantiate the application.
+$app = $container->get(Joomla\CMS\Application\SiteApplication::class);
 
-// set false ini_get('session.use_cookies') and set false headers_sent
-!ini_get('session.use_cookies') && !headers_sent($file, $line);
-
-// activate session
-session_start();
 
 class EmundusHelperEmail extends TestCase
 {
