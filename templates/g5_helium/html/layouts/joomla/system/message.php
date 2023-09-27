26b53a33e6f490401955b6fdfd72a89de2071bc8
Brice <b.hubinet@gmail.com>
2023-09-27 14:26:54 +0200

    Fix J4! alerts

--- templates/g5_helium/html/layouts/joomla/system/message.php
+++ templates/g5_helium/html/layouts/joomla/system/message.php
@@ -1,64 +1,104 @@
 <?php
 
 /**
- * @package   Gantry 5 Theme
- * @author    RocketTheme http://www.rockettheme.com
- * @copyright Copyright (C) 2007 - 2022 RocketTheme, LLC
- * @copyright Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license   GNU/GPLv2 and later
+ * @package         Joomla.Site
+ * @subpackage      Layout
  *
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * @copyright   (C) 2014 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 
-if (version_compare(JVERSION, 4.0, '>')) {
-    include JPATH_ROOT . '/layouts/joomla/system/message.php';
-    return;
-}
+/* @var $displayData array */
+$msgList   = $displayData['msgList'];
+$document  = Factory::getDocument();
+$msgOutput = '';
+$alert     = [
+	CMSApplication::MSG_EMERGENCY => 'danger',
+	CMSApplication::MSG_ALERT     => 'danger',
+	CMSApplication::MSG_CRITICAL  => 'danger',
+	CMSApplication::MSG_ERROR     => 'danger',
+	CMSApplication::MSG_WARNING   => 'warning',
+	CMSApplication::MSG_NOTICE    => 'info',
+	CMSApplication::MSG_INFO      => 'info',
+	CMSApplication::MSG_DEBUG     => 'info',
+	'message'                     => 'success'
+];
 
-/**
- * Joomla 3 version of the system messages.
- */
+// Load JavaScript message titles
+Text::script('ERROR');
+Text::script('MESSAGE');
+Text::script('NOTICE');
+Text::script('WARNING');
 
-$msgList = $displayData['msgList'];
+// Load other Javascript message strings
+Text::script('JCLOSE');
+Text::script('JOK');
+Text::script('JOPEN');
 
+// Alerts progressive enhancement
+$document->getWebAssetManager()
+	->useStyle('webcomponent.joomla-alert')
+	->useScript('messages');
 
-?>
-<div id="system-message-container">
-    <?php if (is_array($msgList) && !empty($msgList)) : ?>
-    <div id="system-message">
-        <?php foreach ($msgList as $type => $msgs) : ?>
-            <?php
-	        switch ($type) {
-                case 'error':
-                    $icon = 'cancel';
-                    break;
-                case 'warning':
-                    $icon = 'error';
-                    break;
-                case 'success':
-                    $icon = 'check_circle';
-                    break;
-                default:
-                    $icon = 'info';
-                    break;
-            }
-            ?>
-            <div class="alert alert-<?php echo $type; ?>">
+if (is_array($msgList) && !empty($msgList)) { ?>
+    <div id="system-message-container" aria-live="polite">
+        <div id="system-message">
+			<?php
+			$messages = [];
 
-                <?php if (!empty($msgs)) : ?>
-                    <span class="material-icons"><?php echo $icon ?></span>
-                    <div>
-                        <?php foreach ($msgs as $msg) : ?>
-                            <p><?php echo $msg; ?></p>
-                        <?php endforeach; ?>
-                    </div>
-                <?php endif; ?>
-            </div>
-        <?php endforeach; ?>
+			foreach ($msgList as $type => $msgs) {
+				switch ($type) {
+					case 'error':
+						$icon = 'cancel';
+						break;
+					case 'warning':
+						$icon = 'error';
+						break;
+					case 'success':
+						$icon = 'check_circle';
+						break;
+					default:
+						$icon = 'info';
+						break;
+				}
+				?>
+                <div class="alert alert-<?php echo $type; ?>">
+
+					<?php if (!empty($msgs)) : ?>
+                        <span class="material-icons"><?php echo $icon ?></span>
+                        <div>
+							<?php foreach ($msgs as $msg) : ?>
+                                <p><?php echo $msg; ?></p>
+							<?php endforeach; ?>
+                        </div>
+					<?php endif; ?>
+                </div>
+
+				<?php
+				// JS loaded messages
+				$messages[] = [$alert[$type] ?? $type => $msgs];
+				// Noscript fallback
+				if (!empty($msgs)) {
+					$msgOutput .= '<div class="alert alert-' . ($alert[$type] ?? $type) . '">';
+					foreach ($msgs as $msg) :
+						$msgOutput .= $msg;
+					endforeach;
+					$msgOutput .= '</div>';
+				}
+			}
+
+			if ($msgOutput !== '') {
+				$msgOutput = '<noscript>' . $msgOutput . '</noscript>';
+			}
+			?>
+        </div>
     </div>
-    <?php endif; ?>
-</div>
+	<?php
+}
+?>
