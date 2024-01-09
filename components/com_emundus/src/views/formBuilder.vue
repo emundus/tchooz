96375409b14a1b2d2898ee7d9c3e8a79c7884b29
Legendre <jeremy.legendre@emundus.fr>
2024-01-09 10:12:19 +0100

    update components

--- components/com_emundus/src/views/formBuilder.vue
+++ components/com_emundus/src/views/formBuilder.vue
@@ -230,7 +230,7 @@ export default {
             icon: 'translate',
             active: false,
             displayed: true,
-            url: '/parametres-globaux'
+            url: '/parametres-globaux?layout=translation&default_menu=2&object=emundus_setup_profiles'
           },
         ],
       },
@@ -255,6 +255,8 @@ export default {
         this.form_id = this.profile_id;
         this.profile_id = 0;
       }
+    } else {
+      this.leftPanel.tabs[2].url += '&data=' + this.profile_id;
     }
 
     this.getFormTitle();
