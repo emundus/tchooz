ba426377c373a078b7b3089186e8d17342c0ba60
Brice <b.hubinet@gmail.com>
2024-02-02 14:59:20 +0100

    Add switch_profile module in vanilla,

--- modules/mod_emundus_calendar_add/helper.php
+++ modules/mod_emundus_calendar_add/helper.php
@@ -1,4 +1,7 @@
 <?php
+
+use Joomla\CMS\Factory;
+
 defined('_JEXEC') or die('Access Deny');
 
 class modEmundusCalendarAddHelper
@@ -6,21 +9,16 @@ class modEmundusCalendarAddHelper
 
 	public function getPrograms()
 	{
-
 		try {
-
-			$db = JFactory::getDbo();
+			$db = Factory::getContainer()->get('DatabaseDriver');
 			$db->setQuery('SELECT code, label FROM #__emundus_setup_programmes');
 
 			return $db->loadObjectList();
-
 		}
 		catch (Exception $e) {
 			die($e->getMessage());
 		}
-
 	}
-
 }
 
 ?>
