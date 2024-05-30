0c99c888d40748b4870315f3d0f7b2c06d32f402
HUBINET Brice <brice.hubinet@emundus.fr>
2024-05-30 11:20:32 +0200

    Fix decision and evaluation iframes

--- components/com_emundus/views/application/tmpl/decision.php
+++ components/com_emundus/views/application/tmpl/decision.php
@@ -13,13 +13,10 @@
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
 
-if (version_compare(JVERSION, '4.0', '>')) {
-	Factory::getApplication()->getSession()->set('application_layout', 'comment');
-}
-else {
-	Factory::getSession()->set('application_layout', 'comment');
-}
+Factory::getApplication()->getSession()->set('application_layout', 'comment');
 
 ?>
 <div class="row">
@@ -27,21 +24,21 @@ else {
         <div class="panel-heading em-container-decision-heading">
             <h3 class="panel-title">
                 <span class="glyphicon glyphicon-check"></span>
-				<?php echo JText::_('COM_EMUNDUS_DECISION'); ?>
+				<?php echo Text::_('COM_EMUNDUS_DECISION'); ?>
 				<?php if (EmundusHelperAccess::asAccessAction(8, 'c', $this->_user->id, $this->fnum)): ?>
                     <a class="  clean" target="_blank"
-                       href="<?php echo JURI::base(); ?>index.php?option=com_emundus&controller=evaluation&task=pdf_decision&user=<?php echo $this->student->id; ?>&fnum=<?php echo $this->fnum; ?>">
+                       href="<?php echo Uri::base(); ?>index.php?option=com_emundus&controller=evaluation&task=pdf_decision&user=<?php echo $this->student->id; ?>&fnum=<?php echo $this->fnum; ?>">
                         <button class="btn btn-default"
-                                data-title="<?php echo JText::_('COM_EMUNDUS_EXPORTS_DOWNLOAD_PDF'); ?>"
+                                data-title="<?php echo Text::_('COM_EMUNDUS_EXPORTS_DOWNLOAD_PDF'); ?>"
                                 data-toggle="tooltip" data-placement="bottom"
-                                title="<?= JText::_('COM_EMUNDUS_EXPORTS_DOWNLOAD_PDF'); ?>"><span
+                                title="<?= Text::_('COM_EMUNDUS_EXPORTS_DOWNLOAD_PDF'); ?>"><span
                                     class="material-icons">file_download</span></button>
                     </a>
 				<?php endif; ?>
                 <div class="em-flex-row">
 					<?php if (!empty($this->url_form)): ?>
                         <a href="<?php echo $this->url_form; ?>" target="_blank" class="em-flex-row"
-                           title="<?php echo JText::_('COM_EMUNDUS_DECISION_OPEN_DECISION_FORM_IN_NEW_TAB_DESC'); ?>"><span
+                           title="<?php echo Text::_('COM_EMUNDUS_DECISION_OPEN_DECISION_FORM_IN_NEW_TAB_DESC'); ?>"><span
                                     class="material-icons">open_in_new</span></a>
 					<?php endif; ?>
                 </div>
@@ -58,34 +55,34 @@ else {
                 <div class="embed-responsive">
                     <div class="form" id="form">
 						<?php if (!empty($this->url_form)): ?>
-                            <div class="holds-iframe"><?php echo JText::_('COM_EMUNDUS_LOADING'); ?></div>
+                            <div class="em-w-100 em-flex-row" style="justify-content: center">
+                                <div class="em-loader"></div>
+                            </div>
                             <iframe id="iframe" class="embed-responsive-item" src="<?php echo $this->url_form; ?>"
-                                    align="left" frameborder="0" height="600" width="100%" scrolling="no"
-                                    marginheight="0" marginwidth="0" onload="resizeIframe(this)"></iframe>
+                                    height="600" width="100%" onload="onLoadIframe(this)"></iframe>
 						<?php else: ?>
-                            <div class="em_no-form"><?php echo JText::_('COM_EMUNDUS_DECISION_NO_DECISION_FORM_SET'); ?></div>
+                            <div class="em_no-form"><?php echo Text::_('COM_EMUNDUS_DECISION_NO_DECISION_FORM_SET'); ?></div>
 						<?php endif; ?>
                     </div>
                 </div>
-                <div class="evaluations" id="evaluations" style="display: none">-----------</div>
+                <div class="decisions" id="decisions" style="display: none">-----------</div>
             </div>
         </div>
     </div>
 </div>
 <script type="text/javascript">
-    $('.fabrikMainError').hide();
-
-    $('iframe').load(function () {
-        $(".holds-iframe").remove();
-    }).show();
-
-    $('#iframe').mouseleave(function () {
+    document.getElementById('iframe').addEventListener('mouseleave', function () {
         resizeIframe(document.getElementById('iframe'));
     });
 
-    $('#iframe').mouseover(function () {
+    document.getElementById('iframe').addEventListener('mouseover', function () {
         resizeIframe(document.getElementById('iframe'));
     });
+
+    function onLoadIframe(iframe) {
+        document.querySelector('.em-loader').classList.add('hidden');
+        resizeIframe(iframe);
+    }
 
     function resizeIframe(obj) {
         obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
@@ -99,14 +96,14 @@ else {
 
     var url_evaluation = '<?php echo $this->url_evaluation; ?>';
 
-    if (url_evaluation != '') {
+    if (url_evaluation !== '') {
         $.ajax({
             type: "GET",
             url: url_evaluation,
             dataType: 'html',
             success: function (data) {
-                $("#evaluations").empty();
-                $("#evaluations").append(data);
+                $("#decisions").empty();
+                $("#decisions").append(data);
             },
             error: function (jqXHR) {
                 console.log(jqXHR.responseText);
