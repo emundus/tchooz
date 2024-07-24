854551810dcfeee26890b2080f075008f357e19e
Laura <laura.grandin@emundus.io>
2024-07-24 17:32:02 +0200

    toogle button style

--- layouts/joomla/tinymce/togglebutton.php
+++ layouts/joomla/tinymce/togglebutton.php
@@ -15,7 +15,7 @@ use Joomla\CMS\Language\Text;
 ?>
 <div class="toggle-editor btn-toolbar float-end clearfix mt-3">
     <div class="btn-group">
-        <button type="button" disabled class="btn btn-secondary js-tiny-toggler-button em-flex-gap-8 em-flex-row">
+        <button type="button" disabled class="btn btn-secondary js-tiny-toggler-button em-flex-gap-8 em-flex-row" style="z-index: 9999;">
             <span class="material-icons-outlined">code</span>
             <?php echo Text::_('PLG_TINY_BUTTON_TOGGLE_EDITOR'); ?>
         </button>
