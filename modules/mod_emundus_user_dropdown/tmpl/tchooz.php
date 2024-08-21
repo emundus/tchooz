53c635b76af247da50d7559e0d5657c9b30f8186
Laura <laura.grandin@emundus.io>
2024-08-21 15:34:15 +0200

    remove the hr when a button is not displayed in the user dropdown  module

--- modules/mod_emundus_user_dropdown/tmpl/tchooz.php
+++ modules/mod_emundus_user_dropdown/tmpl/tchooz.php
@@ -346,7 +346,7 @@ if ($user != null)
 						echo '<option  value="' . $profile->id . "." . '"' . (($user->profile == $profile->id) ? 'selected="selected"' : "") . '>' . trim($profile->label) . '</option>';
 					}
 				}
-				echo '</select></div><br/>';
+				echo '</select></div>';
 			}
 			?>
 
@@ -382,10 +382,8 @@ if ($user != null)
 				}
 			} ?>
 
-            <hr style="width: 100%">
-
 			<?php if ($show_logout == '1') : ?>
-				<?= '<li><a class="logout-button-user em-flex-important em-flex-row em-flex-center" href="' . JURI::base() . 'index.php?option=com_users&task=user.logout&' . JSession::getFormToken() . '=1"><span class="material-symbols-outlined mr-2">logout</span>' . JText::_('COM_EMUNDUS_USER_MENU_LOGOUT_ACTION') . '</a></li>'; ?>
+				<?= '<hr style="width: 100%"><li><a class="logout-button-user em-flex-important em-flex-row em-flex-center" href="' . JURI::base() . 'index.php?option=com_users&task=user.logout&' . JSession::getFormToken() . '=1"><span class="material-symbols-outlined mr-2">logout</span>' . JText::_('COM_EMUNDUS_USER_MENU_LOGOUT_ACTION') . '</a></li>'; ?>
 			<?php endif; ?>
 
         </ul>
