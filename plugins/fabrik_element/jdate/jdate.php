09b7696f68956eb19bdf0ce97125069a5248c8fb
HUBINET Brice <brice.hubinet@emundus.fr>
2024-09-06 12:11:32 +0200

    Update Fabrik to 4.3.1

--- plugins/fabrik_element/jdate/jdate.php
+++ plugins/fabrik_element/jdate/jdate.php
@@ -1715,7 +1715,7 @@ class PlgFabrik_ElementJdate extends PlgFabrik_ElementList
 		$displayData          = new stdClass;
 		$displayData->rows    = $ddData;
 		$displayData->name    = $v;
-		$displayData->class   = "form-select " .$this->filterClass();
+		$displayData->class   = "form-select-sm " .$this->filterClass();
 		$displayData->size    = $size;
 		$displayData->default = $default;
 		$displayData->htmlId  = $this->getHTMLId();
