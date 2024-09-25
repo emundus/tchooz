1049dbe9cd8585c412b76e932a0fc03f4c52170b
jlegendre <jeremy.legendre@emundus.fr>
2024-09-25 17:26:11 +0200

    fix workflow builder installation script

--- administrator/components/com_emundus/scripts/releases/2_1_0.php
+++ administrator/components/com_emundus/scripts/releases/2_1_0.php
@@ -10,10 +10,9 @@
 namespace scripts;
 
 use EmundusHelperUpdate;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Factory;
+require_once(JPATH_SITE . '/administrator/components/com_emundus/models/workflow.php');
 
-class Release2_0_0Installer extends ReleaseInstaller
+class Release2_1_0Installer extends ReleaseInstaller
 {
 	public function __construct()
 	{
@@ -23,10 +22,17 @@ class Release2_0_0Installer extends ReleaseInstaller
 	public function install()
 	{
 		$result = ['status' => false, 'message' => ''];
+		EmundusHelperUpdate::displayMessage('Installation du workflow builder.', 'success');
 
-		require_once(JPATH_ROOT . '/administrator/components/com_emundus/models/workflow.php');
-		$m_workflow = new EmundusModelAdministratorWorkflow();
+		if (!class_exists('EmundusModelAdministratorWorkflow')) {
+			require_once(JPATH_ROOT . '/administrator/components/com_emundus/models/workflow.php');
+		}
+		$m_workflow = new \EmundusModelAdministratorWorkflow();
+
 		$result['status'] = $m_workflow->install();
+		if (!$result['status']) {
+			throw new \Exception('Erreur lors de l\'installation du Workflow Builder.');
+		}
 
 		return $result;
 	}
