157a44e47f36b046e23679bcd569978373bfe39d
HUBINET Brice <brice.hubinet@emundus.fr>
2024-10-22 11:26:37 +0200

    fix: Improve style and fix some bugs related to Flywire payment method

--- modules/mod_emundus_payment/tmpl/select-payment-method.php
+++ modules/mod_emundus_payment/tmpl/select-payment-method.php
@@ -1,18 +1,25 @@
-<a href="/"><?= JText::_('MOD_EMUNDUS_PAYMENT_FLYWIRE_GO_TO_HOMEPAGE') ?></a>
-<div class="container-rounded">
-    <h1 class="em-text-align-center"><?= JText::_('MOD_EMUNDUS_PAYMENT_SELECT_PAYMENT_METHOD') ?></h1>
-    <section id="payment-methods-selector" class="em-flex-row em-w-100">
-		<?php foreach ($params['payment_methods']['payment_method'] as $key => $method): ?>
-            <a href="<?= JFactory::getURI(); ?>&payment_method=<?= $method ?>">
-                <div class="payment-method-option em-pointer em-front-btn em-front-secondary-btn">
-                    <p class="em-text-align-center"
-                       style="color:inherit;"><?= JText::_('MOD_EMUNDUS_PAYMENT_METHOD_' . strtoupper($method)) ?></p>
-					<?php if ($params['payment_methods']['payment_highlighted'][$key]): ?>
-                        <p class="em-ml-4 em-text-align-center" style="color:inherit;"><i>
-                                (<?= JText::_('MOD_EMUNDUS_PAYMENT_HIGHLIGHTED_METHOD') ?>)</i></p>
-					<?php endif; ?>
+<?php
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
+?>
+<a href="/" class="tw-flex tw-items-center tw-text-neutral-900 tw-w-fit-content">
+    <span class="material-symbols-outlined tw-mr-1" aria-hidden="true">navigate_before</span>
+    <?= Text::_('MOD_EMUNDUS_PAYMENT_FLYWIRE_GO_TO_HOMEPAGE') ?>
+</a>
+<div class="tw-rounded tw-bg-white tw-mt-4 tw-p-4">
+    <h1 class="tw-text-center"><?= Text::_('MOD_EMUNDUS_PAYMENT_SELECT_PAYMENT_METHOD') ?></h1>
+    <section id="payment-methods-selector" class="tw-flex tw-items-center">
+        <?php foreach($params['payment_methods']['payment_method'] as $key => $method): ?>
+            <a href="<?=  Factory::getURI(); ?>&payment_method=<?= $method ?>">
+                <div class="payment-method-option tw-cursor-pointer em-front-btn em-front-secondary-btn">
+                    <p class="tw-text-center" style="color:inherit;"><?= Text::_('MOD_EMUNDUS_PAYMENT_METHOD_' . strtoupper($method)) ?></p>
+                    <?php if ($params['payment_methods']['payment_highlighted'][$key]): ?>
+                        <p class="tw-ml-1 tw-text-center" style="color:inherit;"><i> (<?= Text::_('MOD_EMUNDUS_PAYMENT_HIGHLIGHTED_METHOD') ?>)</i></p>
+                    <?php endif; ?>
                 </div>
             </a>
-		<?php endforeach; ?>
+        <?php endforeach; ?>
     </section>
 </div>
