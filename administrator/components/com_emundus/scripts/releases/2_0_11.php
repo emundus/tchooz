e9682299c37096136ebc68646db5c96b8e8e2034
HUBINET Brice <brice.hubinet@emundus.fr>
2024-12-18 09:51:52 +0100

    fix: Display a login button if SAML is enabled and configuration set

---
+++ administrator/components/com_emundus/scripts/releases/2_0_11.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     scripts
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+
+namespace scripts;
+
+use EmundusHelperUpdate;
+
+class Release2_0_11Installer extends ReleaseInstaller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function install()
+	{
+		$query  = $this->db->getQuery(true);
+		$result = ['status' => false, 'message' => ''];
+
+		try
+		{
+			EmundusHelperUpdate::insertTranslationsTag('COM_USERS_LOGIN_WITH', 'Se connecter avec');
+			EmundusHelperUpdate::insertTranslationsTag('COM_USERS_LOGIN_WITH', 'Sign in with', 'override', 0, null, null, 'en-GB');
+
+			$result['status'] = true;
+		}
+		catch (\Exception $e)
+		{
+			$result['message'] = $e->getMessage();
+
+			return $result;
+		}
+
+
+		return $result;
+	}
+}
