5ff956ae99a641efad623fee58f9bf5cbc06c406
HUBINET Brice <brice.hubinet@emundus.fr>
2025-01-29 09:12:11 +0100

    Add civility for btob

--- plugins/fabrik_form/btobform/btobform.php
+++ plugins/fabrik_form/btobform/btobform.php
@@ -304,6 +304,7 @@ class PlgFabrik_FormBtobForm extends plgFabrik_Form
 				}
 
 				$alias_to_fills = [
+					'registration_civility' => $data['jos_emundus_btob_1237_repeat___civility'][$key],
 					'registration_common_name' => $data['jos_emundus_btob_1237_repeat___lastname'][$key],
 					'registration_birth_name' => $data['jos_emundus_btob_1237_repeat___lastname'][$key],
 					'registration_first_name' => $data['jos_emundus_btob_1237_repeat___firstname'][$key],
