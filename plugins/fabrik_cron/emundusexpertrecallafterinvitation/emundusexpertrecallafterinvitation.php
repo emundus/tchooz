f0d3715dde65ed8ea0888f1dbf5e012f776771fa
Brice Hubinet <brice.hubinet@emundus.fr>
2025-02-12 12:42:36 +0000

    Report improvements from 1.39.21

--- plugins/fabrik_cron/emundusexpertrecallafterinvitation/emundusexpertrecallafterinvitation.php
+++ plugins/fabrik_cron/emundusexpertrecallafterinvitation/emundusexpertrecallafterinvitation.php
@@ -9,6 +9,8 @@
  */
 
 // No direct access
+use Joomla\CMS\Log\Log;
+
 defined('_JEXEC') or die('Restricted access');
 
 // Require the abstract plugin class
@@ -47,86 +49,118 @@ class PlgFabrik_Cronemundusexpertrecallafterinvitation extends PlgFabrik_Cron {
     public function process(&$data, &$listModel) {
         require_once(JPATH_ROOT . '/components/com_emundus/models/emails.php');
         $m_emails = new EmundusModelEmails();
-        //Select les invitations avec l'état envoyé en récupérant la date 
+		
         $params = $this->getParams();
         $reminder_day = $params->get('reminder_day', '');
-        $reminder_days = array_map('intval', explode(',', $reminder_day));
-        $model_email = $params->get('reminder_mail_id', '');
-        $form_id = $params->get('reminder_form_id', '');
-        $last_day = $params->get('reminder_last_day', 8);
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query->clear() //Je vérifie si le fnum n'a pas déjà 2 request avec l'état envoyé
-            ->select(array('efr.email','efr.keyid','efrr.parent_id', 'efrr.time_date', 'efrr.fnum_expertise','efrr.nb_relance','efrr.last_notification'))
-            ->from($db->quoteName('#__emundus_files_request', 'efr'))
-            ->leftJoin($db->quoteName('#__emundus_files_request_1614_repeat','efrr'). ' ON efr.id = efrr.parent_id')
-            ->where($db->quoteName('efrr.etat').' = 2 ');
-        $db->setQuery($query);
-        $date_first_invitation = $db->loadObjectList();
 
-        $current_date = new DateTime(); // Date actuelle
-        $emails_sended = [];
-        foreach ($date_first_invitation as $row) {
-            $time_date = new DateTime($row->time_date);
-            $last_notification = new DateTime($row->last_notification);
-            $new_nb_relance = (int)$row->nb_relance + 1;
-            foreach ($reminder_days as $day) {
-                $reminder_date = clone $time_date;
-                $reminder_date->modify("+{$day} days"); // Date de l'invitation + le nombre de jour de relance (si time_date = 26/01 et day = 4 alors $reminder_date = 30/01)
-                if ($current_date->format('Y-m-d') === $reminder_date->format('Y-m-d') && (($current_date->format('Y-m-d') !== $last_notification->format('Y-m-d')) || empty($row->last_notification))) { // Si la date du jour et égale à la date de relance et que la date de dernière relance et différente de la date du jour 
-                    if($day != $last_day){
-                        if(!in_array($row->email, $emails_sended)) // évite d'envoyer un mail à la même adresse
-                        {
-                            $link = 'index.php?option=com_fabrik&c=form&view=form&formid=' . $form_id . '&keyid=' . $row->keyid;
-                            $post = array(
-                                'EXPERT_ACCEPT_LINK_RELATIVE' => $link
-                            );
-            
-                            $sent = $m_emails->sendEmailNoFnum($row->email, $model_email, $post, null, null, null, true); // envoi du mail de relance
-                            if($sent) {
-                                $emails_sended[] = $row->email;
-                            }
-                            $fields = [
-                                $db->quoteName('nb_relance') . ' = ' . $db->quote($new_nb_relance),
-                                $db->quoteName('last_notification') . ' = ' . $db->quote($current_date->format('Y-m-d H:i:s'))
-                            ];
-                        }
-                    }
-                    else{
-                        // fields pour changer l'état à Sans réponse
-                        $fields = [
-                            $db->quoteName('nb_relance') . ' = ' . $db->quote($new_nb_relance),
-                            $db->quoteName('etat') . ' = 5'
-                        ];
-                        // Passage de de l'état à 1 pour envoyer la prochaine invitation
-                        $query->clear()
-                        ->update($db->quoteName('#__emundus_files_request_1614_repeat'))
-                        ->set($db->quoteName('etat') . ' = 1')
-                        ->where($db->quoteName('parent_id') . ' = ' . $db->quote($row->parent_id))
-                        ->andWhere($db->quoteName('etat') . ' = 0')
-                        ->order($db->quoteName('rang') . ' ASC');
-                    try {
-                        $db->setQuery($query);
-                        $db->execute();
-                    } catch (Exception $e) {
-                        JLog::add('Failed to update state of repeat request : ' . $row->parent_id . ' ' .  $e->getMessage(), Log::ERROR, 'com_emundus.error');
-                    }
-                    }
-                    
-                    // Mise à jour des lignes qui ont l'état à 2 pour soit passer l'état à Sans réponse soit incrémenter le nombre de relance et mettre à jour la date de dernière relance
-                    $query->clear()
-                        ->update($db->quoteName('#__emundus_files_request_1614_repeat'))
-                        ->set($fields)
-                        ->where($db->quoteName('parent_id') . ' = ' . $db->quote($row->parent_id))
-                        ->andWhere($db->quoteName('fnum_expertise') . ' LIKE ' . $db->quote($row->fnum_expertise));
-                    try {
-                        $db->setQuery($query);
-                        $db->execute();
-                    } catch (Exception $e) {
-                        JLog::add('Failed to update state and number of relaunch of repeat request : ' . $row->parent_id . ' ' .  $e->getMessage(), Log::ERROR, 'com_emundus.error');
-                    }
-                }
-            }
-        }        
+	    if(!empty($reminder_day))
+	    {
+		    $reminder_days = array_map('intval', explode(',', $reminder_day));
+		    $model_email   = $params->get('reminder_mail_id', '');
+		    $form_id       = $params->get('reminder_form_id', '');
+		    $last_day      = $params->get('reminder_last_day', 8);
+		    $sending_state = 2;
+			$no_reply_state = 5;
+			
+		    $query         = $this->_db->getQuery(true);
+
+		    // Check if files does not have 2 files request at sending state
+		    $query->select(array('efr.email', 'efr.keyid', 'efrr.parent_id', 'efrr.time_date', 'efrr.fnum_expertise', 'efrr.nb_relance', 'efrr.last_notification'))
+			    ->from($this->_db->quoteName('#__emundus_files_request', 'efr'))
+			    ->leftJoin($this->_db->quoteName('#__emundus_files_request_1614_repeat', 'efrr') . ' ON efr.id = efrr.parent_id')
+			    ->where($this->_db->quoteName('efrr.etat') . ' = ' . $sending_state);
+		    $this->_db->setQuery($query);
+		    $date_first_invitation = $this->_db->loadObjectList();
+
+		    $current_date  = new DateTime();
+		    $emails_sended = [];
+
+		    foreach ($date_first_invitation as $row)
+		    {
+			    $time_date         = new DateTime($row->time_date);
+			    $last_notification = new DateTime($row->last_notification);
+			    $new_nb_relance    = (int) $row->nb_relance + 1;
+
+			    foreach ($reminder_days as $day)
+			    {
+				    $reminder_date = clone $time_date;
+
+				    // Date of invitation + number of days of reminder (if time_date = 26/01 and day = 4 then $reminder_date = 30/01)
+				    $reminder_date->modify("+{$day} days");
+
+				    // If the current date is equal to the dunning date and the last dunning date is different from the current date
+				    if (
+						$current_date->format('Y-m-d') === $reminder_date->format('Y-m-d') &&
+						(($current_date->format('Y-m-d') !== $last_notification->format('Y-m-d')) || empty($row->last_notification))
+				    )
+				    {
+					    $fields = [];
+
+					    if ($day != $last_day)
+					    {
+						    if (!in_array($row->email, $emails_sended)) // évite d'envoyer un mail à la même adresse
+						    {
+							    $link = 'index.php?option=com_fabrik&c=form&view=form&formid=' . $form_id . '&keyid=' . $row->keyid;
+							    $post = array(
+								    'EXPERT_ACCEPT_LINK_RELATIVE' => $link
+							    );
+
+							    if ($m_emails->sendEmailNoFnum($row->email, $model_email, $post))
+							    {
+								    $emails_sended[] = $row->email;
+							    }
+
+							    $fields = [
+								    $this->_db->quoteName('nb_relance') . ' = ' . $this->_db->quote($new_nb_relance),
+								    $this->_db->quoteName('last_notification') . ' = ' . $this->_db->quote($current_date->format('Y-m-d H:i:s'))
+							    ];
+						    }
+					    }
+					    else
+					    {
+						    $fields = [
+							    $this->_db->quoteName('nb_relance') . ' = ' . $this->_db->quote($new_nb_relance),
+							    $this->_db->quoteName('etat') . ' = ' . $no_reply_state
+						    ];
+
+						    // Passage de de l'état à 1 pour envoyer la prochaine invitation
+						    $query->clear()
+							    ->update($this->_db->quoteName('#__emundus_files_request_1614_repeat'))
+							    ->set($this->_db->quoteName('etat') . ' = 1')
+							    ->where($this->_db->quoteName('parent_id') . ' = ' . $this->_db->quote($row->parent_id))
+							    ->andWhere($this->_db->quoteName('etat') . ' = 0')
+							    ->order($this->_db->quoteName('rang') . ' ASC');
+						    try
+						    {
+							    $this->_db->setQuery($query);
+							    $this->_db->execute();
+						    }
+						    catch (Exception $e)
+						    {
+							    Log::add('Failed to update state of repeat request : ' . $row->parent_id . ' ' . $e->getMessage(), Log::ERROR, 'com_emundus.error');
+						    }
+					    }
+
+						if(!empty($fields))
+						{
+							$query->clear()
+								->update($this->_db->quoteName('#__emundus_files_request_1614_repeat'))
+								->set($fields)
+								->where($this->_db->quoteName('parent_id') . ' = ' . $this->_db->quote($row->parent_id))
+								->andWhere($this->_db->quoteName('fnum_expertise') . ' LIKE ' . $this->_db->quote($row->fnum_expertise));
+							try
+							{
+								$this->_db->setQuery($query);
+								$this->_db->execute();
+							}
+							catch (Exception $e)
+							{
+								Log::add('Failed to update state and number of relaunch of repeat request : ' . $row->parent_id . ' ' . $e->getMessage(), Log::ERROR, 'com_emundus.error');
+							}
+						}
+				    }
+			    }
+		    }
+	    }
     }
 }
--- plugins/fabrik_list/listcsv/scripts/import_expert.php
+++ plugins/fabrik_list/listcsv/scripts/import_expert.php
@@ -1,105 +1,132 @@
-<?php 
+<?php
 
 use Joomla\CMS\Factory;
-$db = Factory::getDbo();
+use Joomla\CMS\Log\Log;
+
+$app       = Factory::getApplication();
+$db        = Factory::getDbo();
+$query     = $db->getQuery(true);
 $listModel = $this->getModel();
 $formModel = $listModel->getFormModel();
-$data = $formModel->formData;
-$fnum = isset($data['jos_emundus_files_request___fnum']) ? $data['jos_emundus_files_request___fnum'] : '';
-$email = isset($data['jos_emundus_files_request___email']) ? $data['jos_emundus_files_request___email'] : '';
-$firstname = isset($data['jos_emundus_files_request___firstname']) ? $data['jos_emundus_files_request___firstname'] : '';
-$lastname = isset($data['jos_emundus_files_request___lastname']) ? $data['jos_emundus_files_request___lastname'] : '';
-$attachment_id = isset($data['jos_emundus_files_request___attachment_id']) ? $data['jos_emundus_files_request___attachment_id'] : 0;
-$rang = isset($data['jos_emundus_files_request_1614_repeat___rang']) ? $data['jos_emundus_files_request_1614_repeat___rang'] : 0;
-$time_date = 'NULL';
+$data      = $formModel->formData;
+
+$fnum          = $data['jos_emundus_files_request___fnum'] ?? '';
+$email         = $data['jos_emundus_files_request___email'] ?? '';
+$firstname     = $data['jos_emundus_files_request___firstname'] ?? '';
+$lastname      = $data['jos_emundus_files_request___lastname'] ?? '';
+$attachment_id = $data['jos_emundus_files_request___attachment_id'] ?? 0;
+$rang          = $data['jos_emundus_files_request_1614_repeat___rang'] ?? 0;
+$time_date     = 'NULL';
 
 require_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'models' . DS . 'files.php');
-$m_files       = new EmundusModelFiles;
-$fnumInfos = $m_files->getFnumInfos($fnum);
-$student_id = $fnumInfos['applicant_id'];
-$key = md5(date('Y-m-d h:m:i').'::'.$fnum.'::'.$student_id.'::'.$attachment_id.'::'.rand());
+$m_files = new EmundusModelFiles;
+
+$fnumInfos   = $m_files->getFnumInfos($fnum);
+$student_id  = $fnumInfos['applicant_id'];
+$key         = md5(date('Y-m-d h:m:i') . '::' . $fnum . '::' . $student_id . '::' . $attachment_id . '::' . rand());
 $campaign_id = $fnumInfos['campaign_id'];
-$name = $fnumInfos['name'];
+$name        = $fnumInfos['name'];
 
-
-// Vérifiez si l'email existe dans la table `#__emundus_files_request`
-$query = $db->getQuery(true)
-            ->select('id')
-            ->from($db->quoteName('#__emundus_files_request'))
-            ->where('email LIKE ' . $db->quote($email));
+$query->select('id')
+	->from($db->quoteName('#__emundus_files_request'))
+	->where('email LIKE ' . $db->quote($email));
 $db->setQuery($query);
 $existingIds = $db->loadColumn();
 
-
-$query = $db->getQuery(true);
-$query->select('efr.id')
-      ->from($db->quoteName('#__emundus_files_request', 'efr'))
-      ->leftJoin($db->quoteName('#__emundus_files_request_1614_repeat','efrr'). ' ON efr.id = efrr.parent_id')
-      ->where($db->quoteName('efrr.fnum_expertise').' LIKE '. $db->quote($fnum))
-      ->andWhere($db->quoteName('efr.email') . ' LIKE '. $db->quote($email))
-      ->orWhere('efrr.fnum_expertise like '. $db->quote($fnum). ' AND efrr.rang LIKE '. $db->quote($rang));
+$query->clear()
+	->select('efr.id')
+	->from($db->quoteName('#__emundus_files_request', 'efr'))
+	->leftJoin($db->quoteName('#__emundus_files_request_1614_repeat', 'efrr') . ' ON efr.id = efrr.parent_id')
+	->where($db->quoteName('efrr.fnum_expertise') . ' LIKE ' . $db->quote($fnum))
+	->andWhere($db->quoteName('efr.email') . ' LIKE ' . $db->quote($email))
+	->orWhere('efrr.fnum_expertise like ' . $db->quote($fnum) . ' AND efrr.rang LIKE ' . $db->quote($rang));
 $db->setQuery($query);
 $existing = $db->loadResult();
-if(empty($existing)){
-  if (!empty($existingIds)) {
-      // Si un email existe déjà, récupérez son ID
-      $parentId = (int) $existingIds[0];
-  } else {
-      // Sinon, insérez une nouvelle entrée dans `#__emundus_files_request`
-      $query = $db->getQuery(true);
-      $columns = ['fnum', 'email', 'firstname', 'lastname', 'attachment_id','time_date','keyid','student_id','campaign_id'];
-      $values = [
-          $db->quote($fnum),
-          $db->quote($email),
-          $db->quote($firstname),
-          $db->quote($lastname),
-          $attachment_id,
-          $db->quote($time_date),
-          $db->quote($key),
-          $student_id,
-          $campaign_id
-      ];
-      try{
-        $query->insert($db->quoteName('#__emundus_files_request'))
-            ->columns($db->quoteName($columns))
-            ->values(implode(',', $values));
-        $db->setQuery($query);
-        $db->execute();
-      }
-      catch (Exception $e){
-            JLog::add('Failed to insert request for fnum ' . $fnum . ' '. $e->getMessage(), Log::ERROR, 'com_emundus.error');
-      }
-      
-  
-      // Récupérez l'ID inséré
-      $parentId = $db->insertid();
-  }
-  
-  try{
-    $columns = ['parent_id', 'rang', 'fnum_expertise','etat','nom_candidat_expertise'];
-    $values = [$parentId, $rang, $db->quote($fnum),'0',$db->quote($name)];
-    $query = $db->getQuery(true)
-            ->insert($db->quoteName('#__emundus_files_request_1614_repeat'))
-            ->columns($db->quoteName($columns))
-            ->values(implode(',', $values));
-    $db->setQuery($query);
-    $db->execute();
-  }
-  catch (Exception $e)
-  {
-        JLog::add('Failed to insert repeat request for fnum ' . $fnum . ' '. $e->getMessage(), Log::ERROR, 'com_emundus.error');
-  }
-  
+
+$query->clear()
+	->select('fnum')
+	->from($db->quoteName('#__emundus_campaign_candidature'))
+	->where('fnum LIKE ' . $db->quote($fnum));
+$db->setQuery($query);
+$existingFiles = $db->loadResult();
+
+if (empty($existing) && !empty($existingFiles))
+{
+	if (!empty($existingIds))
+	{
+		// Si un email existe déjà, récupérez son ID
+		$parentId = (int) $existingIds[0];
+	}
+	else
+	{
+		// Sinon, insérez une nouvelle entrée dans `#__emundus_files_request`
+		$columns = ['fnum', 'email', 'firstname', 'lastname', 'attachment_id', 'time_date', 'keyid', 'student_id', 'campaign_id'];
+		$values  = [
+			$db->quote($fnum),
+			$db->quote($email),
+			$db->quote($firstname),
+			$db->quote($lastname),
+			$attachment_id,
+			$db->quote($time_date),
+			$db->quote($key),
+			$student_id,
+			$campaign_id
+		];
+
+		try
+		{
+			$query->clear()
+				->insert($db->quoteName('#__emundus_files_request'))
+				->columns($db->quoteName($columns))
+				->values(implode(',', $values));
+			$db->setQuery($query);
+			$db->execute();
+		}
+		catch (Exception $e)
+		{
+			Log::add('Failed to insert request for fnum ' . $fnum . ' ' . $e->getMessage(), Log::ERROR, 'com_emundus.error');
+		}
+
+		// Récupérez l'ID inséré
+		$parentId = $db->insertid();
+	}
+
+	try
+	{
+		$columns = ['parent_id', 'rang', 'fnum_expertise', 'etat', 'nom_candidat_expertise'];
+		$values  = [$parentId, $rang, $db->quote($fnum), '0', $db->quote($name)];
+		$query->clear()
+			->insert($db->quoteName('#__emundus_files_request_1614_repeat'))
+			->columns($db->quoteName($columns))
+			->values(implode(',', $values));
+		$db->setQuery($query);
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		Log::add('Failed to insert repeat request for fnum ' . $fnum . ' ' . $e->getMessage(), Log::ERROR, 'com_emundus.error');
+	}
 }
-else{
- $already_exists[] = "L'expert ".$lastname. " ".$firstname. " existe déjà dans la liste pour le dossier " .$fnum. " ou il y a déjà un expert associé à ce dossier sur le rang ". $rang;
+else
+{
+	$already_exists[] = "L'expert " . $lastname . " " . $firstname . " existe déjà dans la liste pour le dossier " . $fnum . " ou il y a déjà un expert associé à ce dossier sur le rang " . $rang . " ou le numéro de dossier " . $fnum . " n'existe pas";
+	$conditions       = array(
+		$db->quoteName('parent_id') . ' = 0'
+	);
+
+	$query->clear()
+		->delete($db->quoteName('#__emundus_files_request_1614_repeat'))
+		->where($conditions);
+	$db->setQuery($query);
+	$result = $db->execute();
 }
-if(!empty($already_exists)){
- $app = Factory::getApplication();
- foreach($already_exists as $already_exist){
-   $app->enqueueMessage($already_exist);
- }
- 
+
+if (!empty($already_exists))
+{
+	foreach ($already_exists as $already_exist)
+	{
+		$app->enqueueMessage($already_exist);
+	}
 }
 
 return false;
