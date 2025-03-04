22a6897c16becc2697b4c87967d59fba164c2308
bazile.binson <bazile.binson@emundus.fr>
2025-03-04 17:27:18 +0100

    fix: retrieve user_id of expert when accepting request to be able to use it later core#902

--- administrator/components/com_emundus/scripts/releases/2_2_5.php
+++ administrator/components/com_emundus/scripts/releases/2_2_5.php
@@ -31,6 +31,16 @@ class Release2_2_5Installer extends ReleaseInstaller
 		{
 			EmundusHelperUpdate::installExtension('plg_extension_emundus', 'emundus', '', 'plugin', 1, 'extension', '{}');
 
+            $column_added = EmundusHelperUpdate::addColumn('jos_emundus_files_request', 'expert_user_id', 'INT', 11);
+            if ($column_added['status'])
+            {
+                EmundusHelperUpdate::displayMessage('La colonne expert_user_id a été ajoutée à la table jos_emundus_files_request.', 'success');
+            }
+            else
+            {
+                throw new \Exception('Erreur lors de l\'ajout de la colonne expert_user_id à la table jos_emundus_files_request.');
+            }
+
 			$result['status'] = true;
 		}
 		catch (\Exception $e)
