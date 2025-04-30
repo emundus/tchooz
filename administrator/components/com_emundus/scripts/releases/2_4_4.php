e640543d52ea058e04aadecc13d1db5a6d978b8c
HUBINET Brice <brice.hubinet@emundus.fr>
2025-04-30 11:47:49 +0200

    fix: Update jos_emundus_hikashop foreign key to on delete set null, need more refactoring after

--- administrator/components/com_emundus/scripts/releases/2_4_4.php
+++ administrator/components/com_emundus/scripts/releases/2_4_4.php
@@ -24,6 +24,30 @@ class Release2_4_4Installer extends ReleaseInstaller
 			$tasks[] = !empty($m_translations->updateTranslation('COM_USERS_LOGIN_WITH', 'Se connecter avec %s', 'fr-FR', 'override', '', 0, '', 1));
 			$tasks[] = !empty($m_translations->updateTranslation('COM_USERS_LOGIN_WITH', 'Sign in with %s', 'en-GB','override', '', 0, '', 1));
 
+			$query_fk = 'SET FOREIGN_KEY_CHECKS=0;';
+			$this->db->setQuery($query_fk);
+			$this->db->execute();
+
+			// First check if the foreign key exists
+			$query_fk = 'SELECT CONSTRAINT_NAME FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_NAME = "jos_emundus_hikashop" AND COLUMN_NAME = "fnum" AND REFERENCED_TABLE_NAME IS NOT NULL;';
+			$this->db->setQuery($query_fk);
+			$fkExists = $this->db->loadResult();
+
+			if(!empty($fkExists))
+			{
+				$query_fk = 'ALTER TABLE jos_emundus_hikashop DROP FOREIGN KEY '.$fkExists;
+				$this->db->setQuery($query_fk);
+				$tasks[] = $this->db->execute();
+			}
+
+			$query_fk         = 'ALTER TABLE jos_emundus_hikashop ADD CONSTRAINT jos_emundus_hikashop_ibfk_2 FOREIGN KEY (fnum) REFERENCES jos_emundus_campaign_candidature (fnum) ON UPDATE CASCADE ON DELETE SET NULL;';
+			$this->db->setQuery($query_fk);
+			$tasks[] = $this->db->execute();
+
+			$query_fk = 'SET FOREIGN_KEY_CHECKS=1;';
+			$this->db->setQuery($query_fk);
+			$this->db->execute();
+
 			$result['status'] = !in_array(false, $tasks);
 		}
 		catch (\Exception $e)
