cb3569074a34684f8aa78f81102f97619e6ac702
jlegendre <jeremy.legendre@emundus.fr>
2025-07-11 15:18:41 +0200

    fix: replace values in list #1001

--- components/com_emundus/models/files.php
+++ components/com_emundus/models/files.php
@@ -6067,8 +6067,6 @@ class EmundusModelFiles extends JModelLegacy
 
 			if (!empty($groupParams) && $groupParams->repeat_group_button == 1)
 			{
-				error_log(print_r($fabrik_element, true));
-
 				$value[$fabrik_element['id']] = $this->getFabrikValueRepeat($fabrik_element, [$fnum], $params, true, $row_id);
 			}
 			else if ($fabrik_element['plugin'] === 'databasejoin')
