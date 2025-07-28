323971d9da44ad42c5be2b71338b4365e811cc3f
Bazile Binson <bazile.binson@emundus.fr>
2025-07-28 15:38:31 +0200

    fix: improve writing notes retrieval in pv generation tchooz#962

--- components/com_fabrik/views/details/tmpl/celsa/default_deliberation_admissibilite-formation_lvl_4_word.php
+++ components/com_fabrik/views/details/tmpl/celsa/default_deliberation_admissibilite-formation_lvl_4_word.php
@@ -19,19 +19,18 @@ if (!empty($this->fnums))
 	$return['table']['rows'] = array();
 
 	$db    = Factory::getContainer()->get('DatabaseDriver');
-	$query = $db->createQuery();
+    $query = $db->createQuery();
 
-	$query
-		->clear()
-		->select('jee.fnum, jee.is_admissible, jepd.first_name, jepd.last_name')
-		->from('#__emundus_evaluations AS jee')
-		->leftJoin('#__emundus_personal_detail as jepd ON jepd.fnum = jee.fnum')
-		->where('jee.fnum in (' . implode(', ', $this->fnums) . ')')
-		->order('is_admissible DESC');
-
-
-	$db->setQuery($query);
-	$sorted_fnums = $db->loadObjectList();
+    $query->clear()
+        ->select('jee.fnum, jee.is_admissible, if(jepd.first_name is not null, jepd.first_name, jeu.firstname) as first_name, if(jepd.last_name is not null, jepd.last_name, jeu.lastname) as last_name')
+        ->from('#__emundus_evaluations AS jee')
+        ->leftJoin('#__emundus_personal_detail as jepd ON jepd.fnum = jee.fnum')
+        ->leftJoin('#__emundus_campaign_candidature as jecc ON jecc.fnum = jee.fnum')
+        ->leftJoin('#__emundus_users as jeu ON jeu.user_id = jecc.applicant_id')
+        ->where('jee.fnum in (' . implode(', ', $this->fnums) .')')
+        ->order('is_admissible DESC');
+    $db->setQuery($query);
+    $sorted_fnums = $db->loadObjectList();
 
 	$index = 1;
 	foreach ($sorted_fnums as $key => $item)
--- components/com_fabrik/views/details/tmpl/celsa/default_deliberation_admission-formation_lvl_2_word.php
+++ components/com_fabrik/views/details/tmpl/celsa/default_deliberation_admission-formation_lvl_2_word.php
@@ -20,18 +20,18 @@ if (!empty($this->fnums))
 	$return['table']['rows'] = array();
 
 	$db    = Factory::getContainer()->get('DatabaseDriver');
-	$query = $db->createQuery();
+    $query = $db->createQuery();
 
-	$query
-		->clear()
-		->select('fg.fnum, fg.moyenne_note_jures, jepd.first_name, jepd.last_name')
-		->from('#__emundus_final_grade as fg')
-		->leftJoin('#__emundus_personal_detail as jepd ON jepd.fnum = fg.fnum')
-		->where('fg.fnum in (' . implode(', ', $this->fnums) . ')')
-		->order('fg.moyenne_note_jures DESC');
-
-	$db->setQuery($query);
-	$sorted_fnums = $db->loadObjectList();
+    $query->clear()
+        ->select('fg.fnum, fg.moyenne_note_jures, if(jepd.first_name is not null, jepd.first_name, jeu.firstname) as first_name, if(jepd.last_name is not null, jepd.last_name, jeu.lastname) as last_name')
+        ->from('#__emundus_final_grade as fg')
+        ->leftJoin('#__emundus_personal_detail as jepd ON jepd.fnum = fg.fnum')
+        ->leftJoin('#__emundus_campaign_candidature as jecc ON jecc.fnum = fg.fnum')
+        ->leftJoin('#__emundus_users as jeu ON jeu.user_id = jecc.applicant_id')
+        ->where('fg.fnum in (' . implode(', ', $this->fnums) .')')
+        ->order('fg.moyenne_note_jures DESC');
+    $db->setQuery($query);
+    $sorted_fnums = $db->loadObjectList();
 
 	foreach ($sorted_fnums as $key => $item)
 	{
