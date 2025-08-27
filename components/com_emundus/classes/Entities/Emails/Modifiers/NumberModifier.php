6174a0c177aa8133ff99583b1056a2fc1a70972f
Brice Hubinet <brice.hubinet@emundus.fr>
2025-08-27 08:47:57 +0000

    fix: Allow parameters in modifiers #1063

--- components/com_emundus/classes/Entities/Emails/Modifiers/NumberModifier.php
+++ components/com_emundus/classes/Entities/Emails/Modifiers/NumberModifier.php
@@ -11,9 +11,9 @@ namespace Tchooz\Entities\Emails\Modifiers;
 
 use Tchooz\Interfaces\TagModifierInterface;
 
-// TODO: Add parameters to manage decimals, thousands separator, etc.
 class NumberModifier implements TagModifierInterface
 {
+	private array $params = [];
 
 	public function getName(): string
 	{
@@ -25,21 +25,35 @@ class NumberModifier implements TagModifierInterface
 		return 'Number';
 	}
 
-	public function transform(string $value): string
+	public function transform(string $value, array $params = []): string
 	{
 		if (!is_numeric($value)) {
 			$value = \EmundusHelperFabrik::extractNumericValue($value);
 		}
+
+		$decimalCount = $params[0] ?? 2;
+		$decimalSeparator = $params[1] ?? ',';
+		$thousandsSeparator = $params[2] ?? '';
 		
-		return $this->formatAmount($value);
+		return $this->formatAmount($value, (int) $decimalCount, $decimalSeparator, $thousandsSeparator);
 	}
 
-	private function formatAmount(float $value): string
+	private function formatAmount(float $value, int $decimalCount, string $decimalSeparator, string $thousandsSeparator): string
 	{
 		if (fmod($value, 1) === 0.0) {
-			return number_format($value, 0, ',', '');
+			return number_format($value, 0, $decimalSeparator, $thousandsSeparator);
 		}
 
-		return number_format($value, 2, ',', '');
+		return number_format($value, $decimalCount, $decimalSeparator, $thousandsSeparator);
+	}
+
+	public function setParams(array $params): void
+	{
+		$this->params = $params;
+	}
+
+	public function getParams(): array
+	{
+		return $this->params;
 	}
 }
