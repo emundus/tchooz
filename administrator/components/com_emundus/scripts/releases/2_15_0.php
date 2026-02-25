3862ea648d4a0b97e19a7d4443e6f18e42678017
Clément PILOTTE <clement.pilotte@emundus.fr>
2026-02-25 10:58:07 +0000

    fix: update wrong tags values in mailing form #1378

--- administrator/components/com_emundus/scripts/releases/2_15_0.php
+++ administrator/components/com_emundus/scripts/releases/2_15_0.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * @package     scripts
  * @subpackage
@@ -12,6 +11,7 @@
 namespace scripts;
 
 use scripts\ReleaseInstaller;
+use Tchooz\Repositories\Language\LanguageRepository;
 
 class Release2_15_0Installer extends ReleaseInstaller
 {
@@ -81,6 +81,91 @@ class Release2_15_0Installer extends ReleaseInstaller
 			if (!$installed)
 			{
 				$result['message'] .= 'Failed to install orderlist plugin. ';
+			}
+
+			$languageRepository = new LanguageRepository();
+			$url          = 'https://emundus.atlassian.net/wiki/external/ZjQ2MDYzMzc3YjU3NDU1N2FhMWMxMzFkZWFhNTQ1Njc';
+			$translations = [
+				'en-GB' => 'Need help ?',
+				'fr-FR' => "Besoin d'aide ?",
+			];
+
+			foreach ($translations as $langCode => $translation)
+			{
+				$html = "<p style='text-align: right;'><a href='{$url}' target='_blank' rel='noopener noreferrer'>{$translation}</a></p>";
+				$md5  = md5($html);
+
+				$languageRepository->setLangCode($langCode);
+				$tagLettersIntro = $languageRepository->getByTag('SETUP_LETTERS_INTRO');
+				$tagLettersIntro->setOverride($html);
+				$tagLettersIntro->setOriginalText($html);
+				$tagLettersIntro->setOriginalMd5($md5);
+				$tagLettersIntro->setOverrideMd5($md5);
+				$tagLettersIntro->setModifiedDate(new \DateTime());
+
+				$updated = $languageRepository->flush($tagLettersIntro);
+
+				$this->tasks[] = $updated;
+				if (!$updated)
+				{
+					$result['message'] .= "Failed to update SETUP_LETTERS_INTRO tage from jos_emundus_setup_languages table for {$langCode} language code. ";
+				}
+			}
+
+			$query->clear()
+				->select('path')
+				->from($this->db->quoteName('#__menu'))
+				->where($this->db->quoteName('alias') . ' LIKE ' . $this->db->quote('liste-des-alias'));
+
+			$this->db->setQuery($query);
+			$menuLink = $this->db->loadResult();
+
+			if ($menuLink)
+			{
+				$tagApplicant = '$APPLICANT_NAME';
+
+				$htmlCodes = [
+					'en-GB' => "<p>To make this mail dynamic, insert <a href='/en/{$menuLink}' target='_blank' rel='noopener noreferrer'>tags</a> into its construction to add custom information for each candidate. For example, the <em>{$tagApplicant}</em> tag will be replaced with your candidate's name. Hello <em>{$tagApplicant}</em> will become Hello Julian.</p><p>For wysiwyg fields and if the template is a Word file, you will need to insert the tag this way to preserve the content formatting: $&#123;textarea_&lt;ID_FIELD&gt;&#125;$&#123;ID_FIELD&#125;$&#123;textarea_&lt;ID_FIELD&gt;&#125;</p>",
+					'fr-FR' => "<p>Pour rendre ce courrier dynamique, insérer des <a href='/{$menuLink}' target='_blank' rel='noopener noreferrer'>balises</a> dans sa construction afin d’ajouter des informations personnalisées pour chaque candidat. Par exemple, la balise <em>{$tagApplicant}</em> sera remplacée par le nom de votre candidat. Bonjour <em>{$tagApplicant}</em> deviendra Bonjour Julien.</p><p>Pour les champs de type wysiwyg et si le modèle est un fichier word, il faudra insérer la balise ainsi pour conserver la mise en forme du contenu : $&#123;textarea_&lt;ID_CHAMP&gt;&#125;$&#123;ID_CHAMP&#125;$&#123;textarea_&lt;ID_CHAMP&gt;&#125;</p>",
+				];
+
+				foreach ($htmlCodes as $langCode => $htmlCode)
+				{
+					$md5 = md5($htmlCode);
+
+					$languageRepository->setLangCode($langCode);
+					$tagLettersGroup179Intro = $languageRepository->getByTag('SETUP_LETTERS_GROUP_179_INTRO');
+					$tagLettersGroup179Intro->setOverride($htmlCode);
+					$tagLettersGroup179Intro->setOriginalText($htmlCode);
+					$tagLettersGroup179Intro->setOriginalMd5($md5);
+					$tagLettersGroup179Intro->setOverrideMd5($md5);
+					$tagLettersGroup179Intro->setModifiedDate(new \DateTime());
+					$updated       = $languageRepository->flush($tagLettersGroup179Intro);
+					$this->tasks[] = $updated;
+
+					if (!$updated)
+					{
+						$result['message'] .= "Failed to update tag SETUP_LETTERS_GROUP_179_INTRO from jos_emundus_setup_languages table for {$langCode} language code. ";
+					}
+
+					$translation = $languageRepository->getByTag('SETUP_LETTERS_GROUP_185_INTRO');
+
+					if (!empty($translation))
+					{
+						$translation->setOverride($htmlCode);
+						$translation->setOriginalText($htmlCode);
+						$translation->setOriginalMd5($md5);
+						$translation->setOverrideMd5($md5);
+						$translation->setModifiedDate(new \DateTime());
+						$updated       = $languageRepository->flush($translation);
+						$this->tasks[] = $updated;
+
+						if (!$updated)
+						{
+							$result['message'] .= "Failed to update tag SETUP_LETTERS_GROUP_185_INTRO from jos_emundus_languages table for {$langCode} language code. ";
+						}
+					}
+				}
 			}
 
 			$result['status'] = !in_array(false, $this->tasks);
