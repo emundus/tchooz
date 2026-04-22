a807f8694261d6312e6d40a3fe8c27e7bec21ad5
HUBINET Brice <brice.hubinet@emundus.fr>
2026-04-22 15:57:45 +0200

    fix: The filter-sharing permission is now visible to all platform administrators in the group permissions settings #1794

---
+++ administrator/components/com_emundus/scripts/releases/2_19_1.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @package     scripts
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+
+namespace scripts;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Tchooz\Factories\Language\LanguageFactory;
+
+class Release2_19_1Installer extends ReleaseInstaller
+{
+	private array $tasks = [];
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function install()
+	{
+		$result = ['status' => false, 'message' => ''];
+
+		$query = $this->db->createQuery();
+
+		try
+		{
+			$query->clear()
+				->update($this->db->quoteName('#__emundus_setup_actions'))
+				->set($this->db->quoteName('status') . ' = 1')
+				->where($this->db->quoteName('name') . ' = ' . $this->db->quote('share_filters'));
+			$this->db->setQuery($query);
+			$this->tasks[] = $this->db->execute();
+
+			$result['status'] = !in_array(false, $this->tasks);
+		}
+		catch (\Exception $e)
+		{
+			$result['status']  = false;
+			$result['message'] = $e->getMessage();
+		}
+
+		return $result;
+	}
+}
