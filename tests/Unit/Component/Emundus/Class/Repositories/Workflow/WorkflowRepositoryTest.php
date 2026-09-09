db89b04805482fb5f75a47163f97dfbc0e0593a9
Jérémy Legendre <jeremy.legendre@emundus.fr>
2026-09-09 13:13:32 +0000

    fix: contradiction between old workflow model and new repository save #2235

--- tests/Unit/Component/Emundus/Class/Repositories/Workflow/WorkflowRepositoryTest.php
+++ tests/Unit/Component/Emundus/Class/Repositories/Workflow/WorkflowRepositoryTest.php
@@ -3,10 +3,14 @@
 namespace Unit\Component\Emundus\Class\Repositories\Workflow;
 
 use Joomla\Tests\Unit\UnitTestCase;
+use Tchooz\Entities\Payment\ProductEntity;
 use Tchooz\Entities\Workflow\StepEntity;
 use Tchooz\Entities\Workflow\StepTypeEntity;
 use Tchooz\Entities\Workflow\WorkflowEntity;
 use Tchooz\Repositories\Campaigns\CampaignRepository;
+use Tchooz\Repositories\Payment\CurrencyRepository;
+use Tchooz\Repositories\Payment\PaymentRepository;
+use Tchooz\Repositories\Payment\ProductRepository;
 use Tchooz\Repositories\Workflow\StepTypeRepository;
 use Tchooz\Repositories\Workflow\WorkflowRepository;
 
@@ -216,5 +220,106 @@ class WorkflowRepositoryTest extends UnitTestCase
 		} catch (\Exception $e) {
 			$this->fail('Exception occurred during testGetWorkflowsByFnums: ' . $e->getMessage());
 		}
+	}
+
+	/**
+	 * @covers \Tchooz\Repositories\Workflow\WorkflowRepository::duplicate
+	 * @return void
+	 */
+	public function testDuplicateWithPaymentSteps(): void
+	{
+		$paymentStepType = $this->stepTypeRepository->getStepTypeByCode('payment');
+		$this->assertNotNull($paymentStepType, 'Payment step type should exist.');
+
+		$this->db->setQuery(
+			$this->db->createQuery()
+				->update('#__emundus_setup_sync')
+				->set('published = 1')
+				->set('enabled = 1')
+				->where('type = ' . $this->db->quote('sogecommerce'))
+		);
+		$this->db->execute();
+
+		$steps = [
+			new StepEntity(0, 0, 'Application', $this->applicantStepType, 1000, null, [0], 1, 0, 1, 1),
+			new StepEntity(0, 0, 'First payment', $paymentStepType, null, null, [1], 2, 0, 1, 2),
+			new StepEntity(0, 0, 'Balance payment', $paymentStepType, null, null, [2], 3, 0, 1, 3),
+		];
+
+		$workflow = new WorkflowEntity(0, 'Workflow to duplicate', 1, $steps, [$this->programId]);
+		$this->assertTrue($this->repository->save($workflow), 'Workflow should be saved successfully.');
+
+		$paymentRepository = new PaymentRepository();
+		$services = $paymentRepository->getPaymentServices();
+		$this->assertNotEmpty($services, 'At least one payment service should be available.');
+
+		$currencyRepository = new CurrencyRepository();
+		$currency = $currencyRepository->getCurrencyById(1);
+
+		$productRepository = new ProductRepository();
+		$mandatoryProduct = new ProductEntity();
+		$mandatoryProduct->setLabel('Mandatory product');
+		$mandatoryProduct->setCurrency($currency);
+		$mandatoryProduct->setPrice(500.0);
+		$mandatoryProduct->setMandatory(1);
+		$productRepository->flush($mandatoryProduct);
+
+		$optionalProduct = new ProductEntity();
+		$optionalProduct->setLabel('Optional product');
+		$optionalProduct->setCurrency($currency);
+		$optionalProduct->setPrice(20.0);
+		$optionalProduct->setMandatory(0);
+		$productRepository->flush($optionalProduct);
+
+		$firstPaymentStepId = $workflow->getSteps()[1]->getId();
+		$balancePaymentStepId = $workflow->getSteps()[2]->getId();
+
+		$firstPaymentStep = $paymentRepository->getPaymentStepById($firstPaymentStepId);
+		$firstPaymentStep->setDescription('<p>Pay your registration fees</p>');
+		$firstPaymentStep->setSynchronizerId($services[0]->id);
+		$firstPaymentStep->setPaymentMethods($paymentRepository->getPaymentMethods());
+		$firstPaymentStep->setProducts([$mandatoryProduct, $optionalProduct]);
+		$firstPaymentStep->setAdvanceType(1);
+		$firstPaymentStep->setAdvanceAmount(100);
+		$firstPaymentStep->setInstallmentMonthday(5);
+		$rule = new \stdClass();
+		$rule->from_amount = 200;
+		$rule->to_amount = 400;
+		$rule->min_installments = 1;
+		$rule->max_installments = 3;
+		$firstPaymentStep->setInstallmentRules([$rule]);
+		$this->assertTrue($paymentRepository->flushPaymentStep($firstPaymentStep), 'Payment configuration should be saved.');
+
+		$balancePaymentStep = $paymentRepository->getPaymentStepById($balancePaymentStepId);
+		$balancePaymentStep->setSynchronizerId($services[0]->id);
+		$balancePaymentStep->setPaymentMethods($paymentRepository->getPaymentMethods());
+		$balancePaymentStep->setAdjustBalance(1);
+		$balancePaymentStep->setAdjustBalanceStepId($firstPaymentStepId);
+		$this->assertTrue($paymentRepository->flushPaymentStep($balancePaymentStep), 'Balance payment configuration should be saved.');
+
+		$duplicatedWorkflow = $this->repository->duplicate($this->repository->getWorkflowById($workflow->getId()));
+		$this->assertNotNull($duplicatedWorkflow, 'Workflow should be duplicated successfully.');
+		$this->assertCount(3, $duplicatedWorkflow->getSteps(), 'Duplicated workflow should hold the three steps.');
+
+		$duplicatedFirstPaymentStepId = $duplicatedWorkflow->getSteps()[1]->getId();
+		$duplicatedBalancePaymentStepId = $duplicatedWorkflow->getSteps()[2]->getId();
+		$this->assertNotEquals($firstPaymentStepId, $duplicatedFirstPaymentStepId, 'Duplicated payment step should be a new step.');
+
+		$duplicatedFirstPaymentStep = $paymentRepository->getPaymentStepById($duplicatedFirstPaymentStepId);
+		$this->assertEquals($firstPaymentStep->getDescription(), $duplicatedFirstPaymentStep->getDescription(), 'Description should be duplicated.');
+		$this->assertEquals($firstPaymentStep->getSynchronizerId(), $duplicatedFirstPaymentStep->getSynchronizerId(), 'Synchronizer should be duplicated.');
+		$this->assertEquals($firstPaymentStep->getAdvanceType(), $duplicatedFirstPaymentStep->getAdvanceType(), 'Advance type should be duplicated.');
+		$this->assertEquals($firstPaymentStep->getAdvanceAmount(), $duplicatedFirstPaymentStep->getAdvanceAmount(), 'Advance amount should be duplicated.');
+		$this->assertEquals($firstPaymentStep->getInstallmentMonthday(), $duplicatedFirstPaymentStep->getInstallmentMonthday(), 'Installment monthday should be duplicated.');
+		$this->assertCount(count($firstPaymentStep->getPaymentMethods()), $duplicatedFirstPaymentStep->getPaymentMethods(), 'Payment methods should be duplicated.');
+		$this->assertCount(count($firstPaymentStep->getInstallmentRules()), $duplicatedFirstPaymentStep->getInstallmentRules(), 'Installment rules should be duplicated.');
+
+		$duplicatedProductIds = array_map(fn($product) => $product->getId(), $duplicatedFirstPaymentStep->getProducts());
+		$this->assertContains($mandatoryProduct->getId(), $duplicatedProductIds, 'Mandatory product should be duplicated.');
+		$this->assertContains($optionalProduct->getId(), $duplicatedProductIds, 'Optional product should be duplicated.');
+
+		$duplicatedBalancePaymentStep = $paymentRepository->getPaymentStepById($duplicatedBalancePaymentStepId);
+		$this->assertEquals(1, $duplicatedBalancePaymentStep->getAdjustBalance(), 'Adjust balance should be duplicated.');
+		$this->assertEquals($duplicatedFirstPaymentStepId, $duplicatedBalancePaymentStep->getAdjustBalanceStepId(), 'Adjusted step should point to the duplicated step.');
 	}
 }
