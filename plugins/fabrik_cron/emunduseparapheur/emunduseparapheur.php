712ac34382d86c387b9c74f2edc60d220668e8a9
HUBINET Brice <brice.hubinet@emundus.fr>
2026-09-15 17:15:43 +0200

    fix: Fix bug if applicant's path does not exist when eparapheur have a signed attachment

--- plugins/fabrik_cron/emunduseparapheur/emunduseparapheur.php
+++ plugins/fabrik_cron/emunduseparapheur/emunduseparapheur.php
@@ -15,6 +15,7 @@ use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Log\Log;
 use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Filesystem\Folder;
 
 defined('_JEXEC') or die('Restricted access');
 
@@ -109,9 +110,16 @@ class PlgFabrik_Cronemunduseparapheur extends PlgFabrik_Cron {
 							$idDocument = $edossier['data']->payload->documents->principal->identifiant;
 
 							$fnumInfos = $m_files->getFnumInfos($file_request->fnum);
+
+							$directory = JPATH_ROOT . '/images/emundus/files/' . $file_request->student_id;
+							if (!is_dir($directory) && !Folder::create($directory)) {
+								Log::add('Impossible de créer le répertoire ' . $directory, Log::ERROR, 'com_emundus.emundusrecallmissingdoc');
+								continue;
+							}
+
 							do {
 								$nom  = $h_checklist->setAttachmentName($edossier['data']->payload->documents->principal->nom, $attachments_lbl[$file_request->attachment_id]['lbl'], $fnumInfos);
-								$path = JPATH_ROOT.'/images/emundus/files/' . $file_request->student_id . '/' . $nom;
+								$path = $directory . '/' . $nom;
 							} while(file_exists($path));
 
 							$signed_file = $api->getDocumentContent($idDocument,$path);
