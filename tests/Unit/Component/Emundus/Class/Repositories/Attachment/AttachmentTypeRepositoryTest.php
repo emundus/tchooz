9d95a85af3d0d0aac5942fcaa04a1422645e059f
jlegendre <jeremy.legendre@emundus.fr>
2026-09-25 09:29:54 +0200

    feat: unit testGet

--- tests/Unit/Component/Emundus/Class/Repositories/Attachment/AttachmentTypeRepositoryTest.php
+++ tests/Unit/Component/Emundus/Class/Repositories/Attachment/AttachmentTypeRepositoryTest.php
@@ -28,10 +28,12 @@ class AttachmentTypeRepositoryTest extends UnitTestCase
 	 */
 	public function testGet()
 	{
-		$attachmentTypes = $this->repository->get();
+		$limit = 10;
+
+		$attachmentTypes = $this->repository->get([], $limit);
 		$this->assertIsArray($attachmentTypes, 'Expected get() to return an array');
 		// by default, limit is 10
-		$this->assertLessThanOrEqual(10, count($attachmentTypes), 'Expected get() to return at most 10 items');
+		$this->assertLessThanOrEqual(10, count($attachmentTypes), 'Expected get() to return at most ' . $limit .  ' items');
 		// assert that each item is an object AttachmentType
 		foreach ($attachmentTypes as $attachmentType) {
 			$this->assertInstanceOf(\Tchooz\Entities\Attachments\AttachmentType::class, $attachmentType, 'Expected each item to be an instance of AttachmentType');
